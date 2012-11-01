151cd3bc7c69d5da1aee16a3338869246e80ac9a
Timothy McKenna <mckenna.tim@gmail.com>
2012-11-01 16:40:39 +0000

    changed nolists

--- services/getwholelist.php
+++ services/getwholelist.php
@@ -4,6 +4,8 @@ include('tm/ChromePhp.php');
 $repo = $_GET['repo'];
 $list = $_GET['list'];
 
+
+ChromePhp::log('in detwholelist.php');
 //http://10.0.1.18/stuff2get/services/getwholelist.php?repo=tpn&list=groceries
 $sql = "SELECT stuff, id, need FROM `lists`WHERE repo=? AND list=? ORDER BY stuff";
 
